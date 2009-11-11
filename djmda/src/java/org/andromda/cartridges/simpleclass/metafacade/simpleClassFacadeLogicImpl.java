d657d5bb2f4b46821b43d7c2b13eb0e2db9956b9
fede.caceres <fede.caceres@gmail.com>
2009-11-11 21:42:23 +0000

    Agregando otro proyecto basado en el cartdrige_clean, pero intentado costumizarlo y comprenderlo más.

---
+++ djmda/src/java/org/andromda/cartridges/simpleclass/metafacade/simpleClassFacadeLogicImpl.java
@@ -0,0 +1,35 @@
+package org.andromda.cartridges.simpleclass.metafacade;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.simpleclass.metafacade.simpleClassFacade.
+ *
+ * @see org.andromda.cartridges.simpleclass.metafacade.simpleClassFacade
+ */
+public class simpleClassFacadeLogicImpl
+    extends simpleClassFacadeLogic
+{
+
+    public simpleClassFacadeLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+    /**
+     * @see org.andromda.cartridges.simpleclass.metafacade.simpleClassFacade#attrToPy()
+     */
+    protected java.util.Collection handleAttrToPy()
+    {
+        // TODO: put your implementation here.
+        return null;
+    }
+
+    /**
+     * @see org.andromda.cartridges.simpleclass.metafacade.simpleClassFacade#operToPy()
+     */
+    protected java.util.Collection handleOperToPy()
+    {
+        // TODO: put your implementation here.
+        return null;
+    }
+
+}
