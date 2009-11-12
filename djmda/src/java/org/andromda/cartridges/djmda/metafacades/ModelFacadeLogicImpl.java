35da23533ed11ba5e4191836e46df15d780eb026
sergio.sta <sergio.sta@910160c2-ce5f-11de-8d2e-8bf2cb4fc7ae>
2009-11-12 20:46:06 +0000

    Mega commit para terminar de quitar todo lo que es simpleclass y para además hacer que ya genere código que funcione. Ya están:  - El proyecto de Eclipse  - Un borrador avanzado del vsl para generar los modelos  - La lógica básica para generar modelos  - Correcciones mínimas aquí y allá para que todo ande relativamente bien  - Inclusión y exclusión de algunos directorios para mantener todo en orden.
    Ahora hay que ponerse manos a la obra :D
    - Fede.

---
+++ djmda/src/java/org/andromda/cartridges/djmda/metafacades/ModelFacadeLogicImpl.java
@@ -0,0 +1,99 @@
+package org.andromda.cartridges.djmda.metafacades;
+
+
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.andromda.cartridges.djmda.psm.PyFunc;
+import org.andromda.metafacades.uml.AttributeFacade;
+import org.andromda.metafacades.uml.OperationFacade;
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.djmda.metafacades.ModelFacade.
+ *
+ * @see org.andromda.cartridges.djmda.metafacades.ModelFacade
+ */
+public class ModelFacadeLogicImpl
+    extends ModelFacadeLogic
+{
+
+    public ModelFacadeLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+    /**
+     * @see org.andromda.cartridges.djmda.metafacades.ModelFacade#attrToPy()
+     */
+    protected java.util.Collection handleAttrToPy()
+       {
+    	// TODO: put your implementation here.
+    	Collection attrArr = this.getAttributes(); //se obtienen todos los atributos de la clase
+    	ArrayList varArr = new ArrayList();
+    	for (Iterator iterator = attrArr.iterator(); iterator.hasNext();) {
+			AttributeFacade attribute = (AttributeFacade) iterator.next();
+			String variableName =  attribute.getName() + " = None";
+			varArr.add(variableName);
+    	}
+        return varArr;
+    }
+
+
+    /**
+     * @see org.andromda.cartridges.djmda.metafacades.ModelFacade#operToPy()
+     */
+    protected java.util.Collection handleOperToPy()
+    {
+        // TODO: put your implementation here.
+    	Collection operArr = this.getOperations();
+    	ArrayList funcArr = new ArrayList();
+    	String pyArgNames = "";
+    	for (Iterator iterator = operArr.iterator(); iterator.hasNext();) {
+    		OperationFacade operation = (OperationFacade) iterator.next();
+    		String operationName = operation.getName();
+    		String argNames = operation.getArgumentNames();
+    		if(!argNames.equals("")) {
+    			String[] argsNameArr = argNames.split(",");
+    			pyArgNames = ", " + this.toPyFuncVarNames(argsNameArr);
+    		} else {
+    			pyArgNames = "";
+    		}
+    		
+    		PyFunc pyFunc = new PyFunc(operationName, pyArgNames);
+    		funcArr.add(pyFunc);
+    		
+    	}
+        return funcArr;
+    }
+
+    /**
+     * @see org.andromda.cartridges.djmda.metafacades.ModelFacade#toPGSQL()
+     */
+    protected org.andromda.cartridges.djmda.psm.PGSQLTable handleToPGSQL()
+    {
+        // TODO: put your implementation here.
+        return null;
+    }
+    
+    /**
+     * @param argsNameArr
+     * @return
+     */
+    private String toPyFuncVarNames(String[] argsNameArr) {
+    	// TODO: Manejar valores por default :D
+    	// este codigo solo convierte de
+		// x,y,z
+		// a esto:
+		// x, y, z
+    	String pyArgNames = "";
+    	
+		for(int i = 0; i < argsNameArr.length - 1; i++) {
+    		pyArgNames += argsNameArr[i].trim() + ", ";
+    	}
+    	pyArgNames += argsNameArr[argsNameArr.length - 1].trim();
+    	
+    	return pyArgNames;
+    }
+
+}
