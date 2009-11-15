b01b0b9c756695540ec800ea613434e13c33ec2c
fede.caceres <fede.caceres@gmail.com>
2009-11-15 21:24:58 +0000

    Ahora ya generamos el settings del proyecto y tenemos un metafacade para el mismo.

---
+++ djmda/src/java/org/andromda/cartridges/djmda/metafacades/ProjectFacadeLogicImpl.java
@@ -0,0 +1,49 @@
+package org.andromda.cartridges.djmda.metafacades;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.andromda.metafacades.uml.PackageFacade;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.djmda.metafacades.ProjectFacade.
+ *
+ * @see org.andromda.cartridges.djmda.metafacades.ProjectFacade
+ */
+public class ProjectFacadeLogicImpl
+    extends ProjectFacadeLogic
+{
+
+    public ProjectFacadeLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+    /**
+     * @see org.andromda.cartridges.djmda.metafacades.ProjectFacade#prueba()
+     */
+    protected void handlePrueba()
+    {
+        // TODO: put your implementation here.
+    }
+
+    /**
+     * @see org.andromda.cartridges.djmda.metafacades.ProjectFacade#getApps()
+     */
+    protected java.util.Collection handleGetApps()
+    {
+        // TODO: add your implementation here!
+    	ArrayList apps = new ArrayList();
+    	Collection packages = this.getSubPackages();
+    	
+    	for (Iterator iterator = packages.iterator(); iterator.hasNext();) {
+    		PackageFacade pack = (PackageFacade) iterator.next();
+    		if (pack.getStereotypeNames().contains("App"))
+    			apps.add(pack);
+    	}
+    	
+        return (Collection) apps;
+    }
+
+}
