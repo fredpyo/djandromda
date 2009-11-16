e5d844b73a039519d2d29d3164a9520a25faa9a1
sergio.sta <sergio.sta@910160c2-ce5f-11de-8d2e-8bf2cb4fc7ae>
2009-11-16 21:30:32 +0000

    Enumeraciones en progreso... aun no esta listo :D

---
+++ djmda/src/java/org/andromda/cartridges/djmda/metafacades/ChoiceFacadeLogicImpl.java
@@ -0,0 +1,36 @@
+package org.andromda.cartridges.djmda.metafacades;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.andromda.metafacades.uml.EnumerationLiteralFacade;
+
+
+/**
+ * MetafacadeLogic implementation for org.andromda.cartridges.djmda.metafacades.ChoiceFacade.
+ *
+ * @see org.andromda.cartridges.djmda.metafacades.ChoiceFacade
+ */
+public class ChoiceFacadeLogicImpl
+    extends ChoiceFacadeLogic
+{
+
+    public ChoiceFacadeLogicImpl (Object metaObject, String context)
+    {
+        super (metaObject, context);
+    }
+    /**
+     * @see org.andromda.cartridges.djmda.metafacades.ChoiceFacade#getChoices()
+     */
+    protected java.util.Collection handleGetChoices()
+    {
+    	ArrayList choices = new ArrayList();
+    	for (Iterator iterator = this.getLiterals().iterator(); iterator.hasNext();) {
+    		EnumerationLiteralFacade literal = (EnumerationLiteralFacade) iterator.next();
+    		String a[] = {literal.getName().toLowerCase(), literal.getName().toLowerCase()};
+    		choices.add(String.format("(%s, %s)", a));
+    	}
+        return choices;
+    }
+
+}
