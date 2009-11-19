9c2b6c0737a424ca3b58e32d21f690b06985a8d0
fede.caceres@gmail.com <fede.caceres@gmail.com@910160c2-ce5f-11de-8d2e-8bf2cb4fc7ae>
2009-11-19 22:08:44 +0000

    Comentarios agregados sobre la implementación de PyAttrImpl y DjDataTypeImpl. Corrección al atributo type de DjDataType para que sea un ClassifierFacade (así tiene más información además del nombre, permitiendo hacer cosas sobre él como .isEnumeration()).

--- trunk/djmda/src/java/org/andromda/cartridges/djmda/psm/DjDataTypeImpl.java
+++ trunk/djmda/src/java/org/andromda/cartridges/djmda/psm/DjDataTypeImpl.java
@@ -23,12 +23,12 @@ public class DjDataTypeImpl
         super();
     }
 
-    public DjDataTypeImpl(java.lang.String type)
+    public DjDataTypeImpl(org.andromda.metafacades.uml.ClassifierFacade type)
     {
        super(type);
     }
 
-    public DjDataTypeImpl(java.lang.String type, java.util.Collection parameters)
+    public DjDataTypeImpl(org.andromda.metafacades.uml.ClassifierFacade type, java.util.Collection parameters)
     {
         super(type, parameters);
     }
@@ -52,7 +52,7 @@ public class DjDataTypeImpl
     {
     	super();
     	// type
-    	this.type = sourceAttribute.getType().getName();
+    	this.type = sourceAttribute.getType();
     	this.parameters = new ArrayList();
     	// tagged values
     	for (Iterator i = sourceAttribute.getTaggedValues().iterator(); i.hasNext();) {
@@ -72,6 +72,21 @@ public class DjDataTypeImpl
     public java.lang.String toDjango()
     {
         // @todo implement public java.lang.String toDjango()
+    	// TODO: Como dije en PyAttrImpl.java...
+    	// este debería de retornar un string tipo models.XxxxField(xxx = xx, xx = xx)
+    	// en primera instancia el XxxxField va a estar determinado por el atributo this.type
+    	// (el cual puede ser String, Integer, o una clase de enumeración...
+    	// básicamente el getType() del atributo real es almacenado en this.type, por eso es un ClassifierFacade,
+    	// así que podés hacerle un isEnumeration() o getName(), etc, etc, etc...
+    	// En otras palabras, la lógia de la función pyDataType en ModelFacadeLogicImpl va a terminar acá
+    	// ...
+    	// Adicionalmente... si el valor etiquetado djfield está definido con algo como Password o lo que sea, entonces no se
+    	// debe retornar un CharField sino un PasswordField... o algo así
+    	// y luego se interpretan el resto de los valores etiquetados...
+    	// Ah todos estos están almacenados en la colección this.parameters, donde podes iterar entre las instancias de
+    	// la clase DjDataTypeParameter, que tiene dos atributos key y value, que coincidentemente se refieren al nombre del tag y su valor.
+    	// Ojo que el valor está como un tipo de dato Object, y tenés que castear al tipo adecuado... creo... ya no me acuerdo... jajaja
+    	// bueno... eso es todo... chau
         throw new java.lang.UnsupportedOperationException("org.andromda.cartridges.djmda.psm.DjDataType.toDjango() Not implemented!");
     }
 
